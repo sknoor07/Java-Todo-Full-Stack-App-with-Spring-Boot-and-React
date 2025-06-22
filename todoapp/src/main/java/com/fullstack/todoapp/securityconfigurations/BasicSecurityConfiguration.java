b7db83c870538263fa4ea15a07d94eca251a4970
sknoor07 <nooralamshaikh07@gmail.com>
2025-06-22 12:30:31 +0100

    changes in storage of user deatails in backend
    storing user details from in memory configuration to database configuration in backend also added mutiple users

--- todoapp/src/main/java/com/fullstack/todoapp/securityconfigurations/BasicSecurityConfiguration.java
+++ todoapp/src/main/java/com/fullstack/todoapp/securityconfigurations/BasicSecurityConfiguration.java
@@ -1,11 +1,17 @@
 package com.fullstack.todoapp.securityconfigurations;
 
+
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -20,5 +26,26 @@ public class BasicSecurityConfiguration {
 				.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 				.csrf().disable()
 				.build();
+	}
+	
+	/*
+	 * @Bean public DataSource dataSource() { return new
+	 * EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2).addScript(
+	 * JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION).build(); }
+	 */
+	
+	@Bean
+	public UserDetailsService userDetailsService(DataSource dataSource) {
+		var admin=User.withUsername("Noor Alam").password("{noop}1234").roles("ADMIN").build();
+		var user=User.withUsername("Pratham Thummar").password("{noop}1234").roles("User").build();
+		var jdbcUserDetailsManager=new JdbcUserDetailsManager(dataSource);
+		if (!jdbcUserDetailsManager.userExists(admin.getUsername())) {
+	        jdbcUserDetailsManager.createUser(admin);
+	    }
+
+	    if (!jdbcUserDetailsManager.userExists(user.getUsername())) {
+	        jdbcUserDetailsManager.createUser(user);
+	    }
+		return jdbcUserDetailsManager;
 	}
 }
