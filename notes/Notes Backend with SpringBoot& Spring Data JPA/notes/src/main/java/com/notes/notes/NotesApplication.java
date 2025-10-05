57e042cd393b7ae03f0aca01378a54aa6600fab1
sknoor07 <nooralamshaikh07@gmail.com>
2025-10-05 14:32:31 +0100

    feat: Configure CORS to allow requests from the Front End

--- notes/Notes Backend with SpringBoot& Spring Data JPA/notes/src/main/java/com/notes/notes/NotesApplication.java
+++ notes/Notes Backend with SpringBoot& Spring Data JPA/notes/src/main/java/com/notes/notes/NotesApplication.java
@@ -2,12 +2,22 @@ package com.notes.notes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class NotesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NotesApplication.class, args);
+	}
+	
+	public WebMvcConfigurer corsCongigure() {
+		return new WebMvcConfigurer() {
+			public  void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:3000").allowedMethods("*");
+			}
+		}; 
 	}
 
 }
